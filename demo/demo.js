591473e4a55f0bf4a17bba4df131c597a6601c07
this-is-rafa <rafa@ratradio.net>
2017-05-08 12:12:14 -0400

    make sure we don't resize images already smaller than our target size

--- demo/demo.js
+++ demo/demo.js
@@ -5,16 +5,11 @@ var resizeImage = function(event) {
     reader.onload = function() {
         var tempImage = new Image();
         tempImage.src = reader.result;
-
         tempImage.onload = function() {
                 var output = new ImageResizer(tempImage);
                 document.body.appendChild(output);
         }
-
-        
-
     }
-
     reader.readAsDataURL(input.files[0]);
 }
 
--- src/image_resizer.js
+++ src/image_resizer.js
@@ -9,11 +9,15 @@ var ImageResizer = function(img, quality = 0.85, targetSize = 250000, stepSize =
     var destCanvas = document.createElement('canvas');
     var destCanvas2d = destCanvas.getContext('2d');
 
-    canvas.width = output.width * stepSize;
-    canvas.height = output.height * stepSize;
+    canvas.width = output.width;
+    canvas.height = output.height;
     canvas2d.drawImage(output, 0, 0, canvas.width, canvas.height);
 
-    for (var i = 2; ; i++) {
+    // We write the image to check if it's already smaller than our target size.
+    output.src = canvas.toDataURL('image/jpeg', quality);
+    if ( this.checkDataSize(output.src) <= targetSize ) return output;
+
+    for (var i = 1; ; i++) {
         canvas2d.drawImage(canvas, 0, 0, canvas.width * stepSize, canvas.height * stepSize);
     
         destCanvas.width = output.width * Math.pow(stepSize, i)
@@ -22,7 +26,7 @@ var ImageResizer = function(img, quality = 0.85, targetSize = 250000, stepSize =
 
         output.src = destCanvas.toDataURL('image/jpeg', quality);
 
-        if ( this.checkDataSize(output.src) < targetSize ) return output;
+        if ( this.checkDataSize(output.src) <= targetSize ) return output;
     }
 
 };
